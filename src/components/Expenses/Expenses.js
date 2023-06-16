5205257e7559ba1c36c5fca6f8a750ad0742a82d
Bubbleblob <Kylemach168@gmail.com>
2023-06-16 01:57:18 -0700

    Created + finished ExpensesChart and finished Chart and ChartBar

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 
 import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter'
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 function Expenses(props) {
@@ -21,6 +22,7 @@ function Expenses(props) {
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={expensesFilterYear} onSaveExpensesFilterYear={filterChangeHandler}></ExpensesFilter>
+                <ExpensesChart expenses={filteredExpenses}></ExpensesChart>
                 <ExpensesList items={filteredExpenses}></ExpensesList>
             </Card>
         </div>
